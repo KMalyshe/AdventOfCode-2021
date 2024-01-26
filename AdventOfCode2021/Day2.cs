d1ea4d9072b0ab908613fca0894adda76e6b3aa7
KMalyshe <kyryl.malyshev@gmail.com>
2024-01-26 17:13:35 +0100

    naming fix

--- AdventOfCode2021/Day2.cs
+++ AdventOfCode2021/Day2.cs
@@ -3,33 +3,33 @@ class Day2 {
     public static void solve()
     {
         var input = new List<String>(File.ReadAllLines(@"C:\AoCFiles\21Day2.txt"));
-        var hor1 = 0;
-        var dep1 = 0;
-        var hor2 = 0;
-        var dep2 = 0;
+        var horizontalPartOne = 0;
+        var depthPartOne = 0;
+        var horizontalPartTwo = 0;
+        var depthPartTwo = 0;
         var aim = 0;
         for (int i = 0; i<input.Count(); i++)
         {
-            var split = input[i].Split(" ");
-            var val = Int32.Parse(split[1]);
-            if (split[0] == "forward") 
+            var currentLine = input[i].Split(" ");
+            var parsedValue = Int32.Parse(currentLine[1]);
+            if (currentLine[0] == "forward") 
             {
-                hor1 += val;
-                hor2 += val;
-                dep2 += aim*val;
+                horizontalPartOne += parsedValue;
+                horizontalPartTwo += parsedValue;
+                depthPartTwo += aim*parsedValue;
             }
-            else if (split[0] == "down")
+            else if (currentLine[0] == "down")
             {
-                dep1 += val;
-                aim += val;
+                depthPartOne += parsedValue;
+                aim += parsedValue;
             }
-            else if (split[0] == "up") 
+            else if (currentLine[0] == "up") 
             {
-                dep1 -= val;
-                aim -= val;
+                depthPartOne -= parsedValue;
+                aim -= parsedValue;
             }
         }
-        Console.WriteLine("Part 1: " + hor1*dep1);
-        Console.WriteLine("Part 2: " + hor2*dep2);
+        Console.WriteLine("Part 1: " + horizontalPartOne*depthPartOne);
+        Console.WriteLine("Part 2: " + horizontalPartTwo*depthPartTwo);
     }
 }
--- AdventOfCode2021/Day3.cs
+++ AdventOfCode2021/Day3.cs
@@ -6,64 +6,66 @@ class Day3 {
     public static void solve()
     {
         var input = new List<String>(File.ReadAllLines(@"C:\AoCFiles\21Day3.txt"));
-        int[] tracker = new int[input[0].Length];
+        int[] occurenceWeighting = new int[input[0].Length]; 
         for (int i = 0; i<input.Count(); i++)
         {
-            for (int j = 0; j<input[i].Length; j++)
+            for (int j = 0; j<input[i].Length; j++) 
             {
-                if (input[i][j] == '0') tracker[j] -= 1;
-                else tracker[j] += 1;
+                if (input[i][j] == '0') occurenceWeighting[j] -= 1;
+                else occurenceWeighting[j] += 1;
             }
         }
-        string str1 = "";
-        string str2 = "";
-        for (int i = 0; i<tracker.Length; i++)
+        // occurenceWeighting:
+        // If positive: More 1s, If 0: Equal, If <0: More 0s
+        string binaryNumber1 = "";
+        string binaryNumber2 = "";
+        for (int i = 0; i<occurenceWeighting.Length; i++)
         {
-            if (tracker[i] < 0) 
+            if (occurenceWeighting[i] < 0) 
             {
-                str1 += "1"; str2 += "0";
+                binaryNumber1 += "1"; binaryNumber2 += "0";
 
             }
             else
             {
-                str1 += "0"; str2 += "1";
+                binaryNumber1 += "0"; binaryNumber2 += "1";
             }
         }
-        List<string> input2 = new List<string>(input);
-        var balance2 = 0;
-        var balance = 0;
+        List<string> oxygenInputCopy = new List<string>(input);
+        var oxygenOccurenceWeighting = 0;
+        var co2OcurrenceWeighting = 0;
         // Oxygen Calculation
-        for (int i = 0; i<tracker.Length; i++) 
+        for (int i = 0; i<occurenceWeighting.Length; i++) 
         // I don't see a way to do this shorter since the two lists have different sizes, unless calculation and filtering can be combined..
         {   
-            for (int j = 0; j<input2.Count(); j++) //Oxygen Calculation
+            for (int j = 0; j<oxygenInputCopy.Count(); j++) //Oxygen Calculation
             {
-                if (input2.Count() == 1) break;
-                if (input2[j][i] == '1') balance2+=1;
-                else balance2 -= 1;
+                if (oxygenInputCopy.Count() == 1) break;
+                if (oxygenInputCopy[j][i] == '1') oxygenOccurenceWeighting+=1;
+                else oxygenOccurenceWeighting -= 1;
             }
             for (int j = 0; j<input.Count(); j++) // CO2 Calculation
             {
                 if (input.Count() == 1) break;
-                if (input[j][i] == '1') balance+=1;
-                else balance -= 1;
+                if (input[j][i] == '1') co2OcurrenceWeighting+=1;
+                else co2OcurrenceWeighting -= 1;
             }
-            for (int j = 0; j<input2.Count(); j++) // Oxygen Filtering
+            for (int j = 0; j<oxygenInputCopy.Count(); j++) // Oxygen Filtering
             {
-                if (input2.Count() == 1) break;
-                if (balance2 >= 0)
+                if (oxygenInputCopy.Count() == 1) break;
+                if (oxygenOccurenceWeighting >= 0)
                 {
-                    if (input2[j][i] == '0') input2[j] = "x";
+                    if (oxygenInputCopy[j][i] == '0') oxygenInputCopy[j] = "x";
                 }
                 else
                 {
-                    if (input2[j][i] == '1') input2[j] = "x";
+                    if (oxygenInputCopy[j][i] == '1') oxygenInputCopy[j] = "x";
                 }
             }
             for (int j = 0; j<input.Count(); j++) // CO2 Filtering
             {
                 if (input.Count() == 1) break;
-                if (balance >= 0)
+                if (co2OcurrenceWeighting >= 0)
                 {
                     if (input[j][i] == '1') input[j] = "x";
                 }
@@ -72,13 +74,13 @@ class Day3 {
                     if (input[j][i] == '0') input[j] = "x";
                 }
             }
-            balance2 = 0;
-            balance = 0;
-            input2.RemoveAll(x => x == "x");
+            oxygenOccurenceWeighting = 0;
+            co2OcurrenceWeighting = 0;
+            oxygenInputCopy.RemoveAll(x => x == "x");
             input.RemoveAll(x => x == "x");
         }
-        Console.WriteLine("Part 1: " + Convert.ToInt32(str1, 2)*Convert.ToInt32(str2, 2));
-        Console.WriteLine("Part 2: " + Convert.ToInt32(input[0], 2)*Convert.ToInt32(input2[0], 2));
+        Console.WriteLine("Part 1: " + Convert.ToInt32(binaryNumber1, 2)*Convert.ToInt32(binaryNumber2, 2));
+        Console.WriteLine("Part 2: " + Convert.ToInt32(input[0], 2)*Convert.ToInt32(oxygenInputCopy[0], 2));
 
     }
 }
