1dde55521ef61f24919aac33fdbeccd0ee3025b1
KMalyshe <kyryl.malyshev@gmail.com>
2024-02-12 22:05:46 +0100

    day 7 part 1 done

--- AdventOfCode2021/Day7.cs
+++ AdventOfCode2021/Day7.cs
@@ -1,9 +1,34 @@
+using System.Collections.Generic;
+using System;
+using System.ComponentModel.Design;
 class Day7 {
 
     public static void solve()
     {
         var input = new List<String>(File.ReadAllLines(@"C:\AoCFiles\21Day7.txt"));
-        var numberList = input[0].Split(",");
-        
+        List<int> numberListInt = input[0].Split(",").Select(int.Parse).ToList();
+        numberListInt.Sort();
+        if (numberListInt.Count() % 2 == 1) 
+        {
+            int center = (int) numberListInt.Count() / 2;
+            Console.WriteLine("Part 1: " + solveCenter(numberListInt, center));
+        }
+        else
+        {
+            int leftcenter = numberListInt.Count() / 2 - 1;
+            int rightcenter = numberListInt.Count() / 2;
+            Console.WriteLine("Part 1: " + Math.Min(solveCenter(numberListInt, leftcenter), solveCenter(numberListInt, rightcenter)));
+        }
+    }
+
+    public static int solveCenter(List<int> numberList, int index)
+    {
+        int fuelCount = 0;
+        for (int i = 0; i<numberList.Count(); i++)
+        {
+            if (i == index) continue;
+            fuelCount += Math.Abs(numberList[i] - numberList[index]);
+        }
+        return fuelCount;
     }
 }
