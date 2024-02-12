836d15efff14fa3b2eedccf3dafc6a5a5085b405
KMalyshe <kyryl.malyshev@gmail.com>
2024-02-12 21:26:39 +0100

    starting day 7 again, median attempt

---
+++ AdventOfCode2021/Day7.cs
@@ -0,0 +1,9 @@
+class Day7 {
+
+    public static void solve()
+    {
+        var input = new List<String>(File.ReadAllLines(@"C:\AoCFiles\21Day7.txt"));
+        var numberList = input[0].Split(",");
+        
+    }
+}
--- AdventOfCode2021/Driver.cs
+++ AdventOfCode2021/Driver.cs
@@ -1,6 +1,6 @@
 class Driver{
     static void Main() {
     // Can be changed to solve whichever day.
-    Day6.solve();
+    Day7.solve();
     }
 }
