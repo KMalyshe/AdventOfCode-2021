2ac9721b2aed8a2957476f97f2b2ad360e466b2a
KMalyshe <kyryl.malyshev@gmail.com>
2024-02-13 12:50:15 +0100

    day 8 part 1

--- AdventOfCode2021/Day8.cs
+++ AdventOfCode2021/Day8.cs
@@ -4,10 +4,19 @@ class Day8 {
     {
         var input = new List<String>(File.ReadAllLines(@"C:\AoCFiles\21Day8.txt"));
         var rightside = new List<String>();
+        var counter = 0;
         foreach (string line in input)
         {
             rightside.Add(line.Split(" | ")[1]);
         }
-        
+        foreach (string line in rightside)
+        {
+            var numbers = line.Split(" ").ToList();
+            foreach (string number in numbers)
+            {
+                if (((number.Length >= 2) && (number.Length <= 4)) || number.Length == 7) counter++;
+            }
+        }
+        Console.WriteLine(counter);
     }
 }
--- AdventOfCode2021/Driver.cs
+++ AdventOfCode2021/Driver.cs
@@ -1,6 +1,6 @@
 class Driver{
     static void Main() {
     // Can be changed to solve whichever day.
-    Day7.solve();
+    Day8.solve();
     }
 }
