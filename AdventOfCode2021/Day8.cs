1f8b31fa8d8088ed844b34eb759575d436fef96b
KMalyshe <kyryl.malyshev@gmail.com>
2024-02-13 13:59:01 +0100

    least scuffed c# solution

--- AdventOfCode2021/Day8.cs
+++ AdventOfCode2021/Day8.cs
@@ -1,15 +1,44 @@
+using System.Globalization;
+
 class Day8 {
 
     public static void solve()
     {
         var input = new List<String>(File.ReadAllLines(@"C:\AoCFiles\21Day8.txt"));
-        var rightside = new List<String>();
-        var counter = 0;
         foreach (string line in input)
         {
-            rightside.Add(line.Split(" | ")[1]);
+            var delimitSplit = line.Split(" | ");
+            string left = delimitSplit[0];
+            string right = delimitSplit[1];
+            var leftNumbers = left.Split(" ").ToList();
+            var rightNumbers = right.Split(" ").ToList();
+            leftNumbers.Sort((a, b) => a.Length.CompareTo(b.Length)); // make sure 1 is first, 7 is second, etc
+            Dictionary<char, char> wirings = new Dictionary<char, char>();
+
+            // Get top segment by XOR-ing one and seven
+            var one = getChars(leftNumbers[0]);
+            var seven = getChars(leftNumbers[1]);
+            wirings.Add('a', seven.Except(one).ToList()[0]);
+
+            // Get bottom segment by XOR-ing 4 and 9, since we know top segment
+            var four = getChars(leftNumbers[2]);
+            foreach (string number in leftNumbers)
+            {
+                if (number.Length != 6) continue;
+                var fourSet = four.ToHashSet();
+                var nineMaybe = getChars(number).ToHashSet();
+                if (nineMaybe.Intersect(fourSet).ToHashSet().SetEquals(fourSet))
+                {
+                    fourSet.Add(wirings['a']);
+                    wirings.Add('d', nineMaybe.Except(fourSet).ToList()[0]);
+                }
+            }
+
+
         }
-        foreach (string line in rightside)
+        /* Part 1
+        var counter = 0;
+        foreach (string line in rightSide)
         {
             var numbers = line.Split(" ").ToList();
             foreach (string number in numbers)
@@ -18,5 +47,15 @@ class Day8 {
             }
         }
         Console.WriteLine(counter);
+        */
+    }
+    public static List<char> getChars(string str)
+    {
+        List<char> returnList = new List<char>();
+        for (int i = 0; i<str.Length; i++)
+        {
+            returnList.Add(str[i]);
+        }
+        return returnList;
     }
 }
